ad32b0237405d1299aa14464659bd1096ba5c59e
Oleksandr Tkachenko <prosto.batman1234@gmail.com>
2026-02-08 17:41:50 +0100

    icon

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
     return (
         <section className={css.hero}>
           <div className={css.started}>
-            <h1 className={css.title}>Unlock your potential with the best  language tutors</h1>
+            <h1 className={css.title}>Unlock your potential with the best  <span className={css.highlight}>language</span> tutors</h1>
             <p className={css.subtitle}>Embark on an Exciting Language Journey with Expert Language Tutors: Elevate your language proficiency to new heights by connecting with highly qualified and experienced tutors.</p>
             <button className={css.getStartedButton}>Get Started</button>
           </div>
